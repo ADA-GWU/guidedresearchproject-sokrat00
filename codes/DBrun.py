d9944fd87b4d155220cbb0bafa3e8969d3e7d12b
sokrat00 <52068821+sokrat00@users.noreply.github.com>
2023-07-31 00:03:17 +0400

    Update DBrun.py

--- codes/DBrun.py
+++ codes/DBrun.py
@@ -1,72 +1,85 @@
 import mysql.connector
+import psycopg2
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 
-# MySQL connection configuration
-db_config = {
+# Define database connection parameters
+mysql_config = {
     'host': 'localhost',
     'user': 'root',
     'password': 'MySQL1965',
-    'database': 'employees',
+    'database': 'employees'
 }
 
-# 5 queries to be executed
+postgres_config = {
+    'host': 'localhost',
+    'user': 'postgres',
+    'password': 'Postgre1965',
+    'database': 'employees'
+}
+
+# Queries to be executed
 queries = [
-    "SELECT emp_no, COUNT(*) AS count FROM employees GROUP BY emp_no;", 
-    "SELECT * FROM salaries WHERE salary = 94443 OR salary = 59571;", 
-    "SELECT E.*, S.* FROM EMPLOYEES E JOIN SALARIES S ON E.EMP_NO = S.EMP_NO WHERE E.FIRST_NAME = 'Duangkaew';",  
-    "SELECT * FROM TITLES WHERE TITLE LIKE 'senior%';", 
-    "SELECT E.*, T.* FROM EMPLOYEES E JOIN TITLES T ON E.EMP_NO = T.EMP_NO WHERE E.FIRST_NAME = 'Duangkaew';"   
+    "SELECT emp_no, COUNT(*) AS count FROM employees GROUP BY emp_no;",
+    "SELECT * FROM salaries WHERE salary = 94443 OR salary = 59571;",
+    "SELECT E.*, S.* FROM employees E JOIN salaries S ON E.emp_no = S.emp_no WHERE E.first_name = 'Duangkaew';",
+    "SELECT * FROM titles WHERE title LIKE 'senior%';",
+    "SELECT E.*, T.* FROM employees E JOIN titles T ON E.emp_no = T.emp_no WHERE E.first_name = 'Duangkaew';",
 ]
 
 # Function to run queries and collect results
 def run_queries(conn, queries, num_times):
-    results = [[] for _ in range(len(queries))]
-    for _ in range(num_times):
-        for i, query in enumerate(queries):
+    results = []
+    for query in queries:
+        execution_times = []
+        for _ in range(num_times):
             cursor = conn.cursor()
+            start_time = time.time()
             cursor.execute(query)
-            result = cursor.fetchall()
+            # Fetch and process the results, e.g., by using cursor.fetchall()
+            # If you don't need the results, just iterate over the cursor to consume them
+            for _ in cursor:
+                pass
+            end_time = time.time()
+            execution_time = end_time - start_time
+            execution_times.append(execution_time)
             cursor.close()
-            results[i].append(result)
+        results.append(execution_times)
     return results
 
 def main():
-    num_times = 0
-    
+    num_times = 1000
 
     try:
-        # Connect to the database
-        conn = mysql.connector.connect(**db_config)
+        # Connect to MySQL and PostgreSQL databases
+        mysql_conn = mysql.connector.connect(**mysql_config)
+        postgres_conn = psycopg2.connect(**postgres_config)
 
-        # Run the queries and collect results
-        query_results = run_queries(conn, queries, num_times)
+        # Run queries and collect results for MySQL
+        mysql_query_results = run_queries(mysql_conn, queries, num_times)
 
-        # Calculate mean and variance for each query
-        means = [np.mean([len(result) for result in results]) for results in query_results]
-        variances = [np.var([len(result) for result in results]) for results in query_results]
+        # Run queries and collect results for PostgreSQL
+        postgres_query_results = run_queries(postgres_conn, queries, num_times)
 
-        # Build histograms
+        # Close connections
+        mysql_conn.close()
+        postgres_conn.close()
+
+        # Build histograms for each query's execution time in MySQL and PostgreSQL
         for i, query in enumerate(queries):
-            plt.hist([len(result) for result in query_results[i]], bins=20, alpha=0.5, label=f"Query {i+1}")
+            plt.figure(i + 1)
+            plt.hist(mysql_query_results[i], bins=20, alpha=0.5, label="MySQL")
+            plt.hist(postgres_query_results[i], bins=20, alpha=0.5, label="PostgreSQL")
+            plt.legend(loc='upper right')
+            plt.xlabel('Execution Time (seconds)')
+            plt.ylabel('Frequency')
+            plt.title(f'Histogram for Query {i+1}')
+            plt.savefig(f'histogram_query_{i+1}.png')
+            plt.close()
 
-        plt.legend(loc='upper right')
-        plt.xlabel('Number of Rows')
-        plt.ylabel('Frequency')
-        plt.title('Histogram of Query Results')
-        plt.show()
-
-        # Print mean and variance
-        for i, query in enumerate(queries):
-            print(f"Query {i+1} - Mean: {means[i]}, Variance: {variances[i]}")
-
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
-
-    finally:
-        # Close the database connection
-        if conn.is_connected():
-            conn.close()
+    except Exception as e:
+        print(f"Error: {e}")
 
 if __name__ == "__main__":
     main()
